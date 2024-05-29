74e6268539c7f9e9fbd42dfc2fd0481cdeae37de
EC2 Default User <ec2-user@ip-172-31-5-74.ap-northeast-1.compute.internal>
2024-05-29 06:47:05 +0000

    [add] CRUD機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,22 +4,37 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
   def book_params
-    params.require(:book).permit(:tite, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
